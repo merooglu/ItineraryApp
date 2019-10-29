70d507fb8950e0ec2f488372b57cadc7712571e0
Mehmet Eroğlu <>
2019-10-29 12:47:26 +0300

    added readTrips code.

---
+++ Itinerary/Model/Data.swift
@@ -0,0 +1,13 @@
+//
+//  Data.swift
+//  Itinerary
+//
+//  Created by Mehmet Eroğlu on 29.10.2019.
+//  Copyright © 2019 Mehmet Eroğlu. All rights reserved.
+//
+
+import Foundation
+
+class Data {
+    static var tripModels = [TripModel]()
+}
